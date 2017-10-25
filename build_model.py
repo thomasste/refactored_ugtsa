3aacea0474dcc9448b05d1f495b718a8d8bfe6db
Tomasz Steczniewski <tomaszsteczniewski@gmail.com>
2017-10-25 18:54:38 +0000

    build_model.py

---
+++ build_model.py
@@ -0,0 +1,43 @@
+import argparse
+import tensorflow as tf
+from config import games, models
+
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument('game', type=str)
+argument_parser.add_argument('model', type=str)
+argument_parser.add_argument('worker_count', type=int)
+
+args = argument_parser.parse_args()
+
+game_info = games[args.game]
+model = models[args.model]
+model_builder = model(game_info, args.worker_count)
+
+model_builder.build()
+
+with tf.Session() as session:
+    session.run(tf.global_variables_initializer())
+
+    graph_name = '{}__{}__{}'.format(
+        args.game, args.model, args.worker_count)
+    model_name = '{}.0'.format(graph_name)
+
+    saver = tf.train.Saver()
+    saver_def = saver.as_saver_def()
+    saver_save_op = session.graph.get_operation_by_name(
+        saver_def.save_tensor_name[:-2])
+    saver_filename = session.graph.get_tensor_by_name(
+        saver_def.filename_tensor_name)
+
+    print(saver_def.save_tensor_name[:-2])
+    print(saver_def.restore_op_name)
+    print(saver_def.filename_tensor_name)
+
+    session.run(
+        saver_save_op,
+        {saver_filename: 'build/models/{}'.format(model_name)})
+
+    tf.train.write_graph(
+        session.graph.as_graph_def(), 'build/graphs/',
+        '{}.pb'.format(graph_name),
+        as_text=False)
---
+++ config.py
@@ -0,0 +1,49 @@
+from model_builders.basic_model_builder import BasicModelBuilder
+from model_builders.vertical_lstm_model_builder import \
+    VerticalLSTMModelBuilder
+
+games = {
+    'omringa': {
+        'board_shape': [7, 7],
+        'game_state_info_size': 2,
+        'payoff_size': 2,
+        'player_count': 2,
+    }
+}
+
+models = {
+    '25102017_basic':
+        lambda game_info, worker_count: BasicModelBuilder(
+            statistic_size=150,
+            update_size=150,
+            board_shape=game_info['board_shape'],
+            game_state_info_size=game_info['game_state_info_size'],
+            payoff_size=game_info['payoff_size'],
+            player_count=game_info['player_count'],
+            worker_count=worker_count,
+            statistic_filter_shapes=[(2, 2, 16), (2, 2, 32)],
+            statistic_window_shapes=[(1, 2, 2, 1), (1, 2, 2, 1)],
+            statistic_hidden_output_sizes=[150, 150, 150],
+            update_hidden_output_sizes=[150, 150, 150],
+            modified_statistic_lstm_state_sizes=[25, 25, 25],
+            modified_update_hidden_output_sizes=[150, 150, 150],
+            move_rate_hidden_output_sizes=[150, 150, 150]),
+    '25102017_vertical':
+        lambda game_info, worker_count: VerticalLSTMModelBuilder(
+            model_builder=BasicModelBuilder(
+                statistic_size=150,
+                update_size=150,
+                board_shape=game_info['board_shape'],
+                game_state_info_size=game_info['game_state_info_size'],
+                payoff_size=game_info['payoff_size'],
+                player_count=game_info['player_count'],
+                worker_count=worker_count,
+                statistic_filter_shapes=[(2, 2, 16), (2, 2, 32)],
+                statistic_window_shapes=[(1, 2, 2, 1), (1, 2, 2, 1)],
+                statistic_hidden_output_sizes=[150, 150, 150],
+                update_hidden_output_sizes=[150, 150, 150],
+                modified_statistic_lstm_state_sizes=[25, 25, 25],
+                modified_update_hidden_output_sizes=None,
+                move_rate_hidden_output_sizes=[150, 150, 150]),
+            modified_update_lstm_state_sizes=[25, 25, 25]),
+}
